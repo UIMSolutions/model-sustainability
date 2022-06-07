701322eb90a8b85c364d0d65acf507d2483f0436
UIM (Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-06-07 21:56:33 +0200

    Update package.d

--- source/models/sustainability/package.d
+++ source/models/sustainability/package.d
@@ -1,1 +1,18 @@
+module models.sustainability;
 
+// DLang libraries (Phobos)
+public import std.stdio;
+public import std.string;
+public import std.uuid;
+
+// External libraries
+public import vibe.d;
+
+// UIM libraries
+public import uim.core;
+public import uim.oop;
+public import uim.entities;
+
+// model packages
+public import models.sustainability.attributes;
+public import models.sustainability.entities;
