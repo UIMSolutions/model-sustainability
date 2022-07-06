9fc743a661a44ddcf75ef9e6a077b2adc068e87f
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-06 17:22:47 +0200

    Update package.d

--- source/models/sustainability/entities/businesstravels/package.d
+++ source/models/sustainability/entities/businesstravels/package.d
@@ -1,1 +1,4 @@
 module models.sustainability.entities.entities.businesstravels;
+
+public import models.sustainability.entities.entities.businesstravels.businesstravel;
+public import models.sustainability.entities.entities.businesstravels.type;
