90bddac6713b49a746990de731beb5238cd90d4c
UIM (powered by Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-07-06 17:25:07 +0200

    Update package.d

--- source/models/sustainability/entities/emissions/package.d
+++ source/models/sustainability/entities/emissions/package.d
@@ -1,5 +1,8 @@
 module models.sustainability.entities.emissions;
 
+// Main
 public import models.sustainability.entities.emissions.emission;
+
+// Additional
 public import models.sustainability.entities.emissions.factor;
 public import models.sustainability.entities.emissions.source;
